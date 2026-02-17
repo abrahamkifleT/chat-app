18e0cb282239981fb6e11e0b76ab298619f8c789
abraham <abrahamkifle1212@gmail.com>
2026-02-17 13:48:41 +0300

    feat(server): integrate socket.io and implement sendMessage controller

--- server/Controller/messageController.js
+++ server/Controller/messageController.js
@@ -1,57 +1,59 @@
 import User from "../models/User.js";
 import Message from "../models/message.js";
+import cloudinary from "../lib/cloudinary.js";
+import { io, userSocketMap } from "../server.js";
 
 // Get all users except the logged in user
 
-export const getUsersForSidebar = async(req, res) => {
-   try {
-      const userId = req.user._id
-      const filterUser = await User.find({_id: {$ne: userId}}).select("-password");
+export const getUsersForSidebar = async (req, res) => {
+    try {
+        const userId = req.user._id
+        const filterUser = await User.find({ _id: { $ne: userId } }).select("-password");
 
-    //   Count number of messages not seen
-    const unseenMessages = {}
+        //   Count number of messages not seen
+        const unseenMessages = {}
 
-    const promises = filterUser.map(async(user)=> {
-      const messages = await Message.find({
-        senderId: user._id,
-        receiverId: userId,
-        seen: false
-      })
-      if(messages.length > 0){
-        unseenMessages[user._id] = messages.length
-      }
-    })
-    await Promise.all(promises)
-    res.json({success: true, users: filterUser, unseenMessages})
-   } catch (error) {
-      console.log(error.message)
-      res.json({success: false, message: error.message})
-   }
+        const promises = filterUser.map(async (user) => {
+            const messages = await Message.find({
+                senderId: user._id,
+                receiverId: userId,
+                seen: false
+            })
+            if (messages.length > 0) {
+                unseenMessages[user._id] = messages.length
+            }
+        })
+        await Promise.all(promises)
+        res.json({ success: true, users: filterUser, unseenMessages })
+    } catch (error) {
+        console.log(error.message)
+        res.json({ success: false, message: error.message })
+    }
 }
 
 // Gel all message  for selected user
 
-export const getMessages = async(req, res) => {
+export const getMessages = async (req, res) => {
     try {
         const { id: selectedUserId } = req.params
         const myId = req.user._id
 
         const messages = await Message.find({
             $or: [
-                {senderId: myId, receiverId: selectedUserId},
-                {senderId: selectedUserId, receiverId: myId}
+                { senderId: myId, receiverId: selectedUserId },
+                { senderId: selectedUserId, receiverId: myId }
             ]
         })
 
         await Message.updateMany({
             senderId: selectedUserId,
             receiverId: myId
-        }, {seen: true})
+        }, { seen: true })
 
-        res.json({success: true, messages})
+        res.json({ success: true, messages })
     } catch (error) {
         console.log(error.message)
-        res.json({success: false, message: error.message})
+        res.json({ success: false, message: error.message })
     }
 }
 
@@ -60,10 +62,61 @@ export const getMessages = async(req, res) => {
 export const markMessageAsSeen = async (req, res) => {
     try {
         const { id } = req.params
-        await Message.findByIdAndUpdate(id, {seen: true})
-        res.json({success: true})
+        await Message.findByIdAndUpdate(id, { seen: true })
+        res.json({ success: true })
     } catch (error) {
-         console.log(error.message)
-         res.json({success: false, message: error.message})
+        console.log(error.message)
+        res.json({ success: false, message: error.message })
     }
 }
+
+// Send a message to selected user
+export const sendMessage = async (req, res) => {
+    try {
+        const { text, image } = req.body
+        const receiverId = req.params.id
+        const senderId = req.user._id
+
+        let imageUrl;
+        if (image) {
+            const uploadResponse = await cloudinary.uploader.upload(image)
+            imageUrl = uploadResponse.secure_url
+        }
+
+        const newMessage = await Message.create({
+            senderId,
+            receiverId,
+            text,
+            imageUrl
+        })
+
+        // Emit the new message to the receiver's socket
+        const receiverSocketId = userSocketMap[receiverId]
+        if (receiverSocketId) {
+            io.to(receiverSocketId).emit("newMessage", newMessage)
+        }
+
+        res.json({ success: true, message: newMessage })
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- server/server.js
+++ server/server.js
@@ -4,10 +4,40 @@ import cors from "cors"
 import http from "http"
 import { connectDB } from "./lib/db.js";
 import userRoute from "./routes/userRoutes.js"
+import messageRoute from "./routes/messageRoutes.js"
+import {Server} from "socket.io"
 
 const app = express();
 
 const server = http.createServer(app);
+
+// Initialize socket.io
+export const io = new Server(server, {
+    cors: {
+        origin: "*",
+    }
+})
+
+// Store online users
+export const userSocketMap = {} //{userId: sockeId}
+
+// socket.io connection handler
+io.on("connection", (socket)=> {
+    const userId = socket.handshake.query.userId
+    console.log("Connected",userId)
+
+    if(userId) userSocketMap[userId] = socket.id
+
+    // Emit online users to all connected clients
+    io.emit("getOnlineUsers", Object.keys(userSocketMap))
+
+    socket.on("disconnect", () => {
+        console.log("User Disconnected", userId)
+        delete userSocketMap[userId]
+        io.emit("getOnlineUsers", Object.keys(userSocketMap))
+    })
+
+})
 
 // middleware
 app.use(cors());
@@ -17,6 +47,7 @@ app.use(express.json({limit: "4mb"}));
 // route
 app.use("/api/status", (req, res)=> res.send("Server is running"))
 app.use("/api/user", userRoute)
+app.use("/api/messages", messageRoute)
 
 // Connect to mongodb
 await connectDB()
