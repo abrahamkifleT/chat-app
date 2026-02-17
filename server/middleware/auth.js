253e37d99459d1bd48ad3a95e5ab4b048b28a6f6
abraham <abrahamkifle1212@gmail.com>
2026-02-17 11:03:22 +0300

    feat(server): add authentication middleware and checkAuth endpoint

---
+++ server/middleware/auth.js
@@ -0,0 +1,29 @@
+import User from "../models/User";
+
+// Middleware to protect route
+
+import user from "../models/User";
+
+export const protectRoute = async (req, res, next) => {
+    try {
+        const token = req.headers.token;
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+        const user = await User.findById(decoded.userId).select("-password")
+
+        if (!user) return res.json({ success: false, message: "User not found" })
+
+        req.user = user
+        next()
+    } catch (error) {
+        console.log(error.message);
+        res.json({ success: false, message: error.message })
+    }
+}
+
+// Controller to check if user is authenticated
+
+export const checkAuth = async(req, res) => {
+    res.json({success: true, user: req.user})
+}
