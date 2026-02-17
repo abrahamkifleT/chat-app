004f6fe5c86f674b43f9af474ded5cc824e202f8
abraham <abrahamkifle1212@gmail.com>
2026-02-17 11:04:07 +0300

    feat(server): add login and checkAuth controllers

--- server/Controller/userController.js
+++ server/Controller/userController.js
@@ -35,6 +35,35 @@ export const signUp = async (req, res) => {
 }
 
 
+// Controller for login a user
+
+export const login = async(req, res) => {
+  try{
+    const { email, password } = req.body
+    const userData = await User.findOne({email})
+  
+    const isPasswordCorrect = await bcrypt.compare(password, userData.password)
+
+    if(!isPasswordCorrect){
+        return res.status(400).json({message: "Invalid password"})
+    }
+
+    const token = generateToken(userData._id)
+    res.json({success: true, userData, token, message: "Login successful"})
+    
+  }catch(error){
+    console.log(error.message)
+    res.json({success: false, message: error.message})
+  }
+}
+
+// Controller to check if user is authenticated
+
+export const checkAuth = async(req, res) => {
+    res.json({success: true, user: req.user})
+}
+
+
 
 
 
