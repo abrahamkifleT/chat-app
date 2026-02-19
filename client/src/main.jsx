dd6b56c33f20d5b6014c9be6ad8ceb2b59a9b486
abraham <abrahamkifle1212@gmail.com>
2026-02-19 10:54:51 +0300

    feat: Initialize client application with React Router, authentication context, and core page components.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,13 +2,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import { AuthProvider } from '../context/authContext.jsx'
+import { AuthContextProvider } from '../context/AuthContext.jsx'
+
 
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter> 
-  <AuthProvider>
+  <AuthContextProvider>
     <App />
-  </AuthProvider>
+  </AuthContextProvider>
   </BrowserRouter>
 )
