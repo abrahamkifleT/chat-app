6950b5dc02aadc98214f791d574c72861cac3f01
abraham <abrahamkifle1212@gmail.com>
2026-02-21 11:27:46 +0300

    feat(server): configure server for Vercel deployment and conditional local listening

--- server/server.js
+++ server/server.js
@@ -52,9 +52,14 @@ app.use("/api/messages", messageRoute)
 // Connect to mongodb
 await connectDB()
 
+if (process.env.NODE_ENV !== "production") {
+    const PORT = process.env.PORT || 5000
+    server.listen(PORT, () => console.log("server is running on PORT: " + PORT))
+}
 
-const PORT = process.env.PORT || 5000
-server.listen(PORT, () => console.log("server is running on PORT: " + PORT))
+// Export server for vercel
+
+export default server;
 
 
 
