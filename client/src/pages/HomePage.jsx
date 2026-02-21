1a27ef0071132ca870b0df482dd810a5483b0b52
abraham <abrahamkifle1212@gmail.com>
2026-02-21 11:10:47 +0300

    refactor: use ChatContext for selectedUser state and update layout padding in HomePage

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,18 +1,19 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import SideBar from '../components/SideBar'
 import ChatContainer from '../components/ChatContainer'
 import RightSideBar from '../components/RightSideBar'
+import { ChatContext } from '../../context/ChatContext'
 
 const HomePage = () => {
 
-  const [selectedUser, setSelectedUser] = useState(false)
+  const { selectedUser } = useContext(ChatContext)
 
   return (
-    <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]'>
-      <div className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-[100%] grid grid-cols-1 relative ${selectedUser ? "md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]" : "md:grid-cols-2"}`}>
-        <SideBar/>
-        <ChatContainer/>
-        <RightSideBar selectedUser={selectedUser} setSelectedUser={setSelectedUser}/>
+    <div className='border w-full h-screen sm:px-[5%] sm:py-[2%]'>
+      <div className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-full grid grid-cols-1 relative ${selectedUser ? "md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]" : "md:grid-cols-2"}`}>
+        <SideBar />
+        <ChatContainer />
+        <RightSideBar />
       </div>
     </div>
   )
